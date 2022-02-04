c0d4c47e064a5d6e17c33f4a3e7c1bce10b79b0c
karina ivanova <86211129+karina4840@users.noreply.github.com>
2022-02-04 15:23:49 +0800

    port changes

--- app.js
+++ app.js
@@ -148,14 +148,13 @@ app.get('/:customName', function (req, res) {
   });
 });
 
-// ========================================================
-
-app.get('/about', function (req, res) {
-  res.render('views/about.ejs');
-});
-
 // ====================port================================
 
-app.listen(process.env.MONGODB_URI || 3000, function () {
-  console.log('Server started on port 3000.');
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+}); 
